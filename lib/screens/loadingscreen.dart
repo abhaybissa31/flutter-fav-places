1d710b24e3dea20c320f9ae2cb7cca841e528889
abhaybissa31 <theabhaybissa@gmail.com>
2024-07-15 23:36:59 +0530

    Loading screen added

---
+++ lib/screens/loadingscreen.dart
@@ -0,0 +1,56 @@
+import 'dart:async';
+
+import 'package:favorite_places/screens/home.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+class Loadingscreen extends StatefulWidget {
+  @override
+  Splash createState() => Splash();
+}
+
+class Splash extends State<Loadingscreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Timer(
+     const Duration(seconds: 3),
+      () => Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (BuildContext context) => HomeScreen(),
+        ),
+      ),
+    );
+
+    // var assetsImage = const AssetImage(
+    //     'lib/assets/images/placeholder.png'); //<- Creates an object that fetches an image.
+    // var image = Image(
+    //     image: assetsImage,
+    //     height: 300); //<- Creates a widget that displays an image.
+
+    return MaterialApp(
+      home: Scaffold(
+        // appBar: AppBar(
+        //   // title: Text("MyApp"),
+        //   backgroundColor:
+        //       Colors.blue, //<- background color to combine with the picture :-)
+        // ),
+        body: Container(
+          decoration: const BoxDecoration(color: Colors.black),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+              Image.asset("lib/assets/images/maps.png",color: Colors.white,scale:2.8,filterQuality: FilterQuality.high, ),
+              const SizedBox(height: 40,),
+              Text('GeoSpark',style:GoogleFonts.russoOne(fontSize: 60,color: Colors.white, fontWeight: FontWeight.w100), ),
+            ],),
+          ),
+        ), //<- place where the image appears
+      ),
+    );
+  }
+}
