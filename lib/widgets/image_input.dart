57be60f731b2a3a78f854a8a15daa3ebca2b5fff
abhaybissa31 <theabhaybissa@gmail.com>
2024-07-21 17:56:32 +0530

    choice for picking up pic given also inkwell added

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
@@ -8,7 +9,6 @@ class ImageInput extends StatefulWidget {
 
   @override
   State<ImageInput> createState() {
-    // TODO: implement createState
     return _ImageInputState();
   }
 }
@@ -16,29 +16,130 @@ class ImageInput extends StatefulWidget {
 class _ImageInputState extends State<ImageInput> {
   File? _selectedImage;
 
-  void _takePicture() async {
+  void _chooseImagePickerSource() {
+    // ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        backgroundColor: const Color.fromARGB(255, 18, 18, 18),
+        duration: const Duration(seconds: 120),
+        content: Column(
+          children: [
+            const Row(
+              children: [
+                Text(
+                  'Choose a picker:',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  width: 15,
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Column(
+                      children: [
+                        IconButton(
+                          icon: const Icon(CupertinoIcons.camera),
+                          color: Colors.white,
+                          onPressed: () => _takePicture(true),
+                        ),
+                        const Text(
+                          'Click a picture',
+                          style: TextStyle(color: Colors.white),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  width: 100,
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Column(
+                      children: [
+                        IconButton(
+                          icon: const Icon(CupertinoIcons.photo_on_rectangle),
+                          onPressed: () => _takePicture(false),
+                          color: Colors.white,
+                        ),
+                        const Text(
+                          'Choose from Gallery',
+                          style: TextStyle(color: Colors.white),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 40,
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 10,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> _takePicture(bool isCameraSelected) async {
     final imagePicker = ImagePicker();
-    final pickedImage =
-        await imagePicker.pickImage(source: ImageSource.camera,imageQuality: 100,maxWidth: 600);
-    if (pickedImage == null) {
-      return;
+    if (isCameraSelected) {
+      final pickedImage = await imagePicker.pickImage(
+        source: ImageSource.camera,
+        imageQuality: 100,
+        maxWidth: 600,
+        
+      );
+    ScaffoldMessenger.of(context).clearSnackBars();
+      if (pickedImage == null) {
+        return;
+      }
+
+      setState(() {
+        _selectedImage = File(pickedImage.path);
+      });
+    } else {
+      final pickedImage = await imagePicker.pickImage(
+        source: ImageSource.gallery,
+        imageQuality: 100,
+        maxWidth: 600,
+      );
+        ScaffoldMessenger.of(context).clearSnackBars();
+      if (pickedImage == null) {
+        return;
+      }
+
+      setState(() {
+        _selectedImage = File(pickedImage.path);
+      });
     }
-    setState(() {
-      _selectedImage = File(pickedImage.path);
-    });
   }
 
   @override
   Widget build(BuildContext context) {
     Widget content = TextButton.icon(
-      onPressed: _takePicture,
+      onPressed: _chooseImagePickerSource,
       label: const Text("Take a Picture"),
       icon: const Icon(Icons.camera),
     );
 
     if (_selectedImage != null) {
       content = GestureDetector(
-        onTap: _takePicture,
+        onTap: _chooseImagePickerSource,
         child: Image.file(
           _selectedImage!,
           fit: BoxFit.cover,
@@ -50,12 +151,14 @@ class _ImageInputState extends State<ImageInput> {
 
     return Container(
       decoration: BoxDecoration(
-        border:
-            Border.all(width: 1, color: Theme.of(context).colorScheme.primary),
+        border: Border.all(
+          width: 1,
+          color: Theme.of(context).colorScheme.primary,
+        ),
       ),
-      height: 250,
-      alignment: Alignment.center,
+      height: 300,
       width: double.infinity,
+      alignment: Alignment.center,
       child: content,
     );
   }
