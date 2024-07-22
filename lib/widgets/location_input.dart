b9d9424f5b97edf8282c9e1b0f344738db5f4de4
abhaybissa31 <theabhaybissa@gmail.com>
2024-07-23 00:39:30 +0530

    Location package not working but geolocation works

--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-// import 'package:location/location.dart';
+import 'package:location/location.dart';
 
 class LocationInput extends StatefulWidget {
   const LocationInput({super.key});
@@ -11,44 +11,90 @@ class LocationInput extends StatefulWidget {
 }
 
 class _LocationInputState extends State<LocationInput> {
-  // Location? _pickedLocation;
-  // bool _isGettingLoaded = false;
+
+
+
+
+// /// Determine the current position of the device.
+// ///
+// /// When the location services are not enabled or permissions
+// /// are denied the `Future` will return an error.
+// Future<Position> _determinePosition() async {
+//   bool serviceEnabled;
+//   LocationPermission permission;
+
+//   // Test if location services are enabled.
+//   serviceEnabled = await Geolocator.isLocationServiceEnabled();
+//   if (!serviceEnabled) {
+//     // Location services are not enabled don't continue
+//     // accessing the position and request users of the 
+//     // App to enable the location services.
+//     return Future.error('Location services are disabled.');
+//   }
+
+//   permission = await Geolocator.checkPermission();
+//   if (permission == LocationPermission.denied) {
+//     permission = await Geolocator.requestPermission();
+//     if (permission == LocationPermission.denied) {
+//       // Permissions are denied, next time you could try
+//       // requesting permissions again (this is also where
+//       // Android's shouldShowRequestPermissionRationale 
+//       // returned true. According to Android guidelines
+//       // your App should show an explanatory UI now.
+//       return Future.error('Location permissions are denied');
+//     }
+//   }
   
-  // void _getCurrentLocation() async {
-  //   Location location = Location();
+//   if (permission == LocationPermission.deniedForever) {
+//     // Permissions are denied forever, handle appropriately. 
+//     return Future.error(
+//       'Location permissions are permanently denied, we cannot request permissions.');
+//   } 
 
-  //   bool serviceEnabled;
-  //   PermissionStatus permissionGranted;
-  //   LocationData locationData;
+//   // When we reach here, permissions are granted and we can
+//   // continue accessing the position of the device.
+//   return await Geolocator.getCurrentPosition();
+// }
 
-  //   serviceEnabled = await location.serviceEnabled();
-  //   if (!serviceEnabled) {
-  //     serviceEnabled = await location.requestService();
-  //     if (!serviceEnabled) {
-  //       return;
-  //     }
-  //   }
 
-  //   permissionGranted = await location.hasPermission();
-  //   if (permissionGranted == PermissionStatus.denied) {
-  //     permissionGranted = await location.requestPermission();
-  //     if (permissionGranted != PermissionStatus.granted) {
-  //       return;
-  //     }
-  //   }
-  //   setState(() {
-  //     _isGettingLoaded = true;
-  //   });
+  Location? _pickedLocation;
+  bool _isGettingLoaded = false;
+  
+  void _getCurrentLocation() async {
+    Location location = Location();
 
-  //   locationData = await location.getLocation();
-  //   setState(() {
-  //     _isGettingLoaded = false;
-  //   });
-  //   print('-----------------------latitude');
-  //   print(locationData.latitude);
-  //   print('-------------------------longitude');
-  //   print(locationData.longitude);
-  // }
+    bool serviceEnabled;
+    PermissionStatus permissionGranted;
+    LocationData locationData;
+
+    serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        return;
+      }
+    }
+
+    permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+    setState(() {
+      _isGettingLoaded = true;
+    });
+
+    locationData = await location.getLocation();
+    setState(() {
+      _isGettingLoaded = false;
+    });
+    print('-----------------------latitude');
+    print(locationData.latitude);
+    print('-------------------------longitude');
+    print(locationData.longitude);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -82,7 +128,7 @@ class _LocationInputState extends State<LocationInput> {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             TextButton.icon(
-              onPressed: () {},
+              onPressed: _getCurrentLocation,
               label: const Text("Get current location"),
               icon: const Icon(Icons.location_on),
             ),
